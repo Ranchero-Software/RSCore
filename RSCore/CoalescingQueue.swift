d4d76ec675078457fe1cef1b9dd7ea2ff932942e
Brent Simmons <brent@ranchero.com>
2018-07-28 17:38:11 -0700

    Add maxInterval to CoalescingQueue.

--- RSCore/CoalescingQueue.swift
+++ RSCore/CoalescingQueue.swift
@@ -38,26 +38,33 @@ struct QueueCall: Equatable {
 
 @objc public final class CoalescingQueue: NSObject {
 
-	public static let standard = CoalescingQueue(name: "Standard")
+	public static let standard = CoalescingQueue(name: "Standard", interval: 0.05, maxInterval: 0.1)
 	public let name: String
 	private let interval: TimeInterval
+	private let maxInterval: TimeInterval
+	private var lastCallTime = Date.distantFuture
 	private var timer: Timer? = nil
 	private var calls = [QueueCall]()
 
-	public init(name: String, interval: TimeInterval = 0.05) {
+	public init(name: String, interval: TimeInterval = 0.05, maxInterval: TimeInterval = 2.0) {
 
 		self.name = name
 		self.interval = interval
+		self.maxInterval = maxInterval
 	}
 
 	public func add(_ target: AnyObject, _ selector: Selector) {
 
 		let queueCall = QueueCall(target: target, selector: selector)
 		add(queueCall)
+		if Date().timeIntervalSince1970 - lastCallTime.timeIntervalSince1970 > maxInterval {
+			timerDidFire(nil)
+		}
 	}
 
 	@objc func timerDidFire(_ sender: Any?) {
 
+		lastCallTime = Date()
 		let callsToMake = calls // Make a copy in case calls are added to the queue while performing calls.
 		resetCalls()
 		callsToMake.forEach { $0.perform() }
