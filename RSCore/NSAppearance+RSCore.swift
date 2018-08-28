797369a5c372c2886392e806112bfa3c903a5345
Daniel Jalkut <jalkut@red-sweater.com>
2018-08-28 18:09:41 -0400

    Since RSCore will require macOS 10.14 SDK or greater, remove legacy support path.

--- RSCore/NSAppearance+RSCore.swift
+++ RSCore/NSAppearance+RSCore.swift
@@ -13,37 +13,17 @@ extension NSAppearance {
 	public var isDarkMode: Bool {
 		let isDarkMode: Bool
 
-		// Until we are building against the 10.14 SDK, we have to dynamically lookup and message NSAppearance.bestMatch,
-		// and also define .darkAqua literally since it's not present in the 10.13 SDK.
-		#if swift(>=4.2)
-			if #available(macOS 10.14, *) {
-				if self.effectiveAppearance.bestMatch(from: [.darkAqua, .aqua]) == .darkAqua {
-					isDarkMode = true
-				}
-				else {
-					isDarkMode = false
-				}
+		if #available(macOS 10.14, *) {
+			if self.bestMatch(from: [.darkAqua, .aqua]) == .darkAqua {
+				isDarkMode = true
 			}
 			else {
 				isDarkMode = false
 			}
-		#else
-			if #available(macOS 10.14, *) {
-				let darkAquaAppearanceName = NSAppearance.Name(rawValue: "NSAppearanceNameDarkAqua") // .darkAqua
-				let appearances = [darkAquaAppearanceName, .aqua]
-				let bestMatchSelector = NSSelectorFromString("bestMatchFromAppearancesWithNames:")
-				let currentMode: NSAppearance.Name? = self.perform(bestMatchSelector, with: appearances)?.takeUnretainedValue() as? NSAppearance.Name
-				if currentMode == darkAquaAppearanceName {
-					isDarkMode = true
-				}
-				else {
-					isDarkMode = false
-				}
-			}
-			else {
-				isDarkMode = false
-			}
-		#endif
+		}
+		else {
+			isDarkMode = false
+		}
 		return isDarkMode
 	}
 }
