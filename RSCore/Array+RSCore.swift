4682329b70eba64f94500885307f99ed8cbcc938
Maurice Parker <mo@vincode.io>
2019-05-15 11:51:59 -0500

    added chunk to Array to split large arrays into usable chunks

--- RSCore/Array+RSCore.swift
+++ RSCore/Array+RSCore.swift
@@ -17,4 +17,11 @@ public extension Array {
 		}
 		return self[index]
 	}
+	
+	func chunked(into size: Int) -> [[Element]] {
+		return stride(from: 0, to: count, by: size).map {
+			Array(self[$0 ..< Swift.min($0 + size, count)])
+		}
+	}
+	
 }
