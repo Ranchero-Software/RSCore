d640a2310b96a0a3d4d34c49c08c7bce195d0762
Maurice Parker <mo@vincode.io>
2019-09-12 11:23:55 -0500

    Added optional dismiss action

--- UIKit/UIViewController-Extensions.swift
+++ UIKit/UIViewController-Extensions.swift
@@ -35,15 +35,18 @@ extension UIViewController {
 	
 	// MARK: Error Handling
 	
-	public func presentError(_ error: Error) {
+	public func presentError(_ error: Error, dismiss: (() -> Void)? = nil) {
 		let errorTitle = NSLocalizedString("Error", comment: "Error")
-		presentError(title: errorTitle, message: error.localizedDescription)
+		presentError(title: errorTitle, message: error.localizedDescription, dismiss: dismiss)
 	}
 	
-	public func presentError(title: String, message: String) {
+	public func presentError(title: String, message: String, dismiss: (() -> Void)? = nil) {
 		let alertController = UIAlertController(title: title, message: message, preferredStyle: .alert)
 		let dismissTitle = NSLocalizedString("Dismiss", comment: "Dismiss")
-		alertController.addAction(UIAlertAction(title: dismissTitle, style: .default))
+		let dismissAction = UIAlertAction(title: dismissTitle, style: .default) { _ in
+			dismiss?()
+		}
+		alertController.addAction(dismissAction)
 		self.present(alertController, animated: true, completion: nil)
 	}
 	
