1adcf4087b6cb5806c81d8cdfd68d73858408ae5
Maurice Parker <mo@vincode.io>
2019-09-13 15:45:49 -0500

    Add ManagedResourceFile to observe for reloading and queueing saves

---
+++ RSCore/ManagedResourceFile.swift
@@ -0,0 +1,84 @@
+//
+//  ManagedResourceFile.swift
+//  RSCore
+//
+//  Created by Maurice Parker on 9/13/19.
+//  Copyright © 2019 Ranchero Software, LLC. All rights reserved.
+//
+
+import Foundation
+
+public final class ManagedResourceFile: NSObject, NSFilePresenter {
+	
+	private var isDirty = false {
+		didSet {
+			queueSaveToDiskIfNeeded()
+		}
+	}
+	
+	private var isLoading = false
+	private let fileURL: URL
+	private let operationQueue: OperationQueue
+	private let saveQueue = CoalescingQueue(name: "Save Queue", interval: 1.0)
+
+	private let loadCallback: () -> Void
+	private let reloadCallback: () -> Void
+	private let saveCallback: () -> Void
+
+	public var presentedItemURL: URL? {
+		return fileURL
+	}
+	
+	public var presentedItemOperationQueue: OperationQueue {
+		return operationQueue
+	}
+	
+	public init(fileURL: URL, load: @escaping () -> Void, reload: @escaping () -> Void, save: @escaping () -> Void) {
+		
+		self.fileURL = fileURL
+		self.loadCallback = load
+		self.reloadCallback = reload
+		self.saveCallback = save
+		
+		operationQueue = OperationQueue()
+		operationQueue.maxConcurrentOperationCount = 1
+	
+		super.init()
+		
+		NSFileCoordinator.addFilePresenter(self)
+	}
+	
+	public func presentedItemDidChange() {
+		DispatchQueue.main.async {
+			self.reloadCallback()
+		}
+	}
+	
+	public func markAsDirty() {
+		if !isLoading {
+			isDirty = true
+		}
+	}
+	
+	public func queueSaveToDiskIfNeeded() {
+		saveQueue.add(self, #selector(saveToDiskIfNeeded))
+	}
+
+	public func load() {
+		isLoading = true
+		loadCallback()
+		isLoading = false
+	}
+	
+}
+
+private extension ManagedResourceFile {
+	
+	@objc func saveToDiskIfNeeded() {
+		if isDirty {
+			isDirty = false
+			saveCallback()
+		}
+	}
+
+}
