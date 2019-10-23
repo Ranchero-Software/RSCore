fa16a5b1a0bc45bb6aee6145e1095446a84f386b
Maurice Parker <mo@vincode.io>
2019-10-22 19:19:31 -0500

    Remove the ability to deselect

--- UIKit/UITableView-Extensions.swift
+++ UIKit/UITableView-Extensions.swift
@@ -13,15 +13,12 @@ extension UITableView {
 	/**
 	Selects a row and scrolls it to the middle if it is not visible
 	*/
-	public func selectRowAndScrollIfNotVisible(at indexPath: IndexPath, animated: Bool, deselect: Bool = false) {
+	public func selectRowAndScrollIfNotVisible(at indexPath: IndexPath, animated: Bool) {
 		if let visibleIndexPaths = indexPathsForRows(in: safeAreaLayoutGuide.layoutFrame) {
 			if visibleIndexPaths.contains(indexPath) && cellCompletelyVisable(indexPath) {
 				selectRow(at: indexPath, animated: animated, scrollPosition: .none)
 			} else {
 				selectRow(at: indexPath, animated: animated, scrollPosition: .middle)
-			}
-			if deselect {
-				selectRow(at: nil, animated: animated, scrollPosition: .none)
 			}
 		}
 	}
