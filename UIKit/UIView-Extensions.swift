972ff3237f819a2250e0bc1ca2814bafe328fa69
Maurice Parker <mo@vincode.io>
2019-11-07 14:28:44 -0600

    Add utility function to convert views to images

--- UIKit/UIView-Extensions.swift
+++ UIKit/UIView-Extensions.swift
@@ -29,4 +29,11 @@ extension UIView {
 		
 	}
 	
+    public func asImage() -> UIImage {
+        let renderer = UIGraphicsImageRenderer(bounds: bounds)
+        return renderer.image { rendererContext in
+            layer.render(in: rendererContext.cgContext)
+        }
+    }
+	
 }
