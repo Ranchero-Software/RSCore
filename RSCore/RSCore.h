a26b2b743e13cc6b364139ebb6200dbb4345b81c
Nate Weaver <wevah@derailer.org>
2020-01-08 16:14:56 -0600

    Remove Objective-C NSView+RSCore and RSGeometry

--- RSCore/RSCore.h
+++ RSCore/RSCore.h
@@ -26,11 +26,8 @@
 
 #import <RSCore/NSPasteboard+RSCore.h>
 #import <RSCore/NSTableView+RSCore.h>
-#import <RSCore/NSView+RSCore.h>
 
 #import <RSCore/NSImage+RSCore.h>
-
-#import <RSCore/RSGeometry.h>
 
 #import <RSCore/NSAppleEventDescriptor+RSCore.h>
 #import <RSCore/SendToBlogEditorApp.h>
