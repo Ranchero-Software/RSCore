506d9b1ba1d8996462847afa5cd801efe846422d
Nate Weaver <wevah@derailer.org>
2020-01-08 16:15:58 -0600

    Add documentation comments

--- RSCore/AppKit/Geometry.swift
+++ RSCore/AppKit/Geometry.swift
@@ -9,6 +9,11 @@
 import Foundation
 
 public extension CGRect {
+	/**
+	Centers a rectangle vertically in another rectangle.
+	- Parameter containerRect: The rectangle in which to be centered.
+	- Returns: A new rectangle, cenetered vertically in `containerRect`, with the same size as the source rectangle.
+	*/
 	func centeredVertically(in containerRect: CGRect) -> CGRect {
 		var r = self;
 		r.origin.y = containerRect.midY - (r.height / 2.0);
@@ -17,6 +22,11 @@ public extension CGRect {
 		return r;
 	}
 
+	/**
+	Centers a rectangle horizontally in another rectangle.
+	- Parameter containerRect: The rectangle in which to be centered.
+	- Returns: A new rectangle, cenetered horizontally in `containerRect`, with the same size as the source rectangle.
+	*/
 	func centeredHorizontally(in containerRect: CGRect) -> CGRect {
 		var r = self;
 		r.origin.x = containerRect.midX - (r.width / 2.0);
@@ -25,6 +35,11 @@ public extension CGRect {
 		return r;
 	}
 
+	/**
+	Centers a rectangle in another rectangle.
+	- Parameter containerRect: The rectangle in which to be centered.
+	- Returns: A new rectangle, cenetered both horizontally and vertically in `containerRect`, with the same size as the source rectangle.
+	*/
 	func centered(in containerRect: CGRect) -> CGRect {
 		return self.centeredHorizontally(in: self.centeredVertically(in: containerRect))
 	}
