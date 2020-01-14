b098748be09187b6dcfc3edf5193e92b8120b6ae
Nate Weaver <wevah@derailer.org>
2020-01-14 17:27:30 -0600

    Convert NNW-used methods from NSString+RSCore to Swift

--- RSCore/String+RSCore.swift
+++ RSCore/String+RSCore.swift
@@ -34,3 +34,111 @@ public extension String {
     }
 	
 }
+
+public extension String {
+
+	var md5HashData: Data {
+		NSData(data: self.data(using: .utf8)!).rs_md5Hash()
+	}
+
+	var md5HashString: String {
+		NSData(data: self.md5HashData).rs_hexadecimalString()
+	}
+
+	/// Trims leading and trailing whitespace, and collapses other whitespace into a single space.
+	var collapsingWhitespace: String {
+		var dest = self
+		dest = dest.trimmingCharacters(in: CharacterSet.whitespacesAndNewlines)
+		return dest.replacingOccurrences(of: "\\s+", with: " ", options: .regularExpression)
+	}
+
+	/// Trims whitespace from the beginning and end of the string.
+	var trimmingWhitespace: String {
+		self.trimmingCharacters(in: CharacterSet.whitespacesAndNewlines)
+	}
+
+	/// Returns `true` if the string contains any character from a set.
+	private func containsAnyCharacter(from charset: CharacterSet) -> Bool {
+		return self.rangeOfCharacter(from: charset) != nil
+	}
+
+	/// Returns `true` if a string may be an IPv6 URL.
+	private var mayBeIPv6URL: Bool {
+		self.range(of: "\\[[0-9a-fA-F:]+\\]", options: .regularExpression) != nil
+	}
+
+	/// Returns `true` if the string may be a URL.
+	var mayBeURL: Bool {
+
+		let s = self.trimmingWhitespace
+
+		if s.isEmpty || (!s.contains(".") && !s.mayBeIPv6URL) {
+			return false
+		}
+
+		let banned = CharacterSet.whitespacesAndNewlines.union(.controlCharacters).union(.illegalCharacters)
+
+		if s.containsAnyCharacter(from: banned) {
+			return false
+		}
+
+		return true
+
+	}
+
+
+	/// Removes a prefix from the beginning of a string.
+	/// - Parameters:
+	///   - prefix: The prefix to remove
+	///   - caseSensitive: `true` if the prefix should be matched case-sensitively.
+	/// - Returns: A new string with the prefix removed.
+	func strippingPrefix(_ prefix: String, caseSensitive: Bool = false) -> String {
+		let options: String.CompareOptions = caseSensitive ? .anchored : [.anchored, .caseInsensitive]
+		return self.replacingOccurrences(of: prefix, with: "", options: options)
+	}
+
+	/// A copy of an HTML string converted to plain text.
+	///
+	/// Replaces `p`, `blockquote`, `div`, `br`, and `li` tags with varying quantities
+	/// of newlines, and guarantees no more than two consecutive newlines.
+	var convertedToPlainText: String {
+		if !self.contains("<") {
+			return self
+		}
+
+		var preflight = self
+
+		let options: String.CompareOptions = [.regularExpression, .caseInsensitive]
+		preflight = preflight.replacingOccurrences(of: "</?blockquote>|</p>", with: "\n\n", options: options)
+		preflight = preflight.replacingOccurrences(of: "<p>|</?div>|<br(?: ?/)?>|</li>", with: "\n", options: options)
+
+		var s = String()
+		s.reserveCapacity(preflight.count)
+		var level = 0
+
+		for char in preflight {
+			if char == "<" {
+				level += 1
+			} else if char == ">" {
+				level -= 1
+			} else if level == 0 {
+				s.append(char)
+			}
+		}
+
+		return s.replacingOccurrences(of: "\\n{3,}", with: "\n\n", options: .regularExpression)
+	}
+
+	func caseInsensitiveContains(_ string: String) -> Bool {
+		return self.range(of: string, options: .caseInsensitive) != nil
+	}
+
+	var escapingSpecialXMLCharacters: String {
+		CFXMLCreateStringByEscapingEntities(kCFAllocatorDefault, self as CFString, nil) as String
+	}
+
+	var strippingHTTPorHTTPSScheme: String {
+		self.strippingPrefix("http://").strippingPrefix("https://")
+	}
+
+}
