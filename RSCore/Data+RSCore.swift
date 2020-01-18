79524969689a21d4abf791a8f22cf36941e142c1
Nate Weaver <wevah@derailer.org>
2020-01-18 00:27:29 -0600

    Data+RSCore cleanup
    - Conditionally import CryptoKit.
    - Capitalize MD5.
    - Use a private enum for all image signatures.
    - Delete now-unused String.Encoding extension.
    - Update documentation comments.
    - Delete NSData+RSCore.

--- RSCore/Data+RSCore.swift
+++ RSCore/Data+RSCore.swift
@@ -7,13 +7,15 @@
 //
 
 import Foundation
+#if canImport(CryptoKit)
 import CryptoKit
+#endif
 import CommonCrypto
 
 public extension Data {
 
-	/// Compute the MD5 hash of the data.
-	var md5Hash: Data {
+	/// The MD5 hash of the data.
+	var MD5Hash: Data {
 
 		if #available(macOS 10.15, *) {
 			let digest = Insecure.MD5.hash(data: self)
@@ -31,39 +33,61 @@ public extension Data {
 
 	}
 
-	var md5String: String? {
-		return md5Hash.hexadecimalString
+	/// The MD5 has of the data, as a hexadecimal string.
+	var MD5String: String? {
+		return MD5Hash.hexadecimalString
 	}
 
-	/// http://www.w3.org/TR/PNG/#5PNG-file-signature : "The first eight bytes of a PNG datastream always contain the following (decimal) values: 137 80 78 71 13 10 26 10"
 
-	private static let pngSignature = Data([137, 80, 78, 71, 13, 10, 26, 10])
+	/// Image signature constants.
+	private enum ImageSignature {
 
+		/// The signature for PNG data.
+		///
+		/// [PNG signature](http://www.w3.org/TR/PNG/#5PNG-file-signature)\:
+		/// "The first eight bytes of a PNG datastream always contain the following (decimal) values:
+		/// ```
+		/// 137 80 78 71 13 10 26 10
+		/// ```
+		static let PNG = Data([137, 80, 78, 71, 13, 10, 26, 10])
+
+		/// The signature for GIF 89a data.
+		///
+		/// [http://www.onicos.com/staff/iz/formats/gif.html](http://www.onicos.com/staff/iz/formats/gif.html)
+		static let GIF89 = "GIF89a".data(using: .ascii)
+		/// The signature for GIF 87a data.
+		///
+		/// [http://www.onicos.com/staff/iz/formats/gif.html](http://www.onicos.com/staff/iz/formats/gif.html)
+		static let GIF87 = "GIF87a".data(using: .ascii)
+
+		/// The signature for standard JPEG data.
+		///
+		/// JPEG signatures start at byte 6.
+		static let JFIF = "JFIF".data(using: .ascii)
+
+		/// The signature for Exif JPEG data.
+		///
+		/// JPEG signatures start at byte 6.
+		static let Exif = "Exif".data(using: .ascii)
+	}
 
 	/// Returns `true` if the data begins with the PNG signature.
 	var isPNG: Bool {
-		return prefix(8) == .pngSignature
+		return prefix(8) == ImageSignature.PNG
 	}
-
-
-	private static let gif89Signature = "GIF89a".data(using: .ascii)
-	private static let gif87Signature = "GIF87a".data(using: .ascii)
 
 	/// Returns `true` if the data begins with a valid GIF signature.
 	///
-	/// [http://www.onicos.com/staff/iz/formats/gif.html](http://www.onicos.com/staff/iz/formats/gif.html)
 	var isGIF: Bool {
 		let prefix = self.prefix(6)
-		return prefix == .gif89Signature || prefix == .gif87Signature
+		return prefix == ImageSignature.GIF89 || prefix == ImageSignature.GIF87
 	}
 
-	private static let jpegSignature = "JFIF".data(using: .ascii)
-	private static let exifSignature = "Exif".data(using: .ascii)
 
 	/// Returns `true` if the data contains a valid JPEG signature.
 	var isJPEG: Bool {
 		let signature = self[6..<10]
-		return signature == .jpegSignature || signature == .exifSignature
+		return signature == ImageSignature.JFIF || signature == ImageSignature.Exif
 	}
 
 	/// Constants for `isProbablyHTML`.
@@ -72,6 +96,7 @@ public extension Data {
 		static let lessThan = "<".utf8.first!
 		static let greaterThan = ">".utf8.first!
 
+		/// Tags in UTF-8/ASCII format.
 		enum UTF8 {
 			static let lowercaseHTML = "html".data(using: .utf8)!
 			static let lowercaseBody = "body".data(using: .utf8)!
@@ -79,6 +104,7 @@ public extension Data {
 			static let uppercaseBody = "HTML".data(using: .utf8)!
 		}
 
+		/// Tags in UTF-16 format.
 		enum UTF16 {
 			static let lowercaseHTML = "html".data(using: .utf16LittleEndian)!
 			static let lowercaseBody = "body".data(using: .utf16LittleEndian)!
@@ -113,11 +139,13 @@ public extension Data {
 		return false
 	}
 
+	/// A representation of the data as a hexadecimal string.
 	var hexadecimalString: String? {
 		if count == 0 {
 			return nil
 		}
 
+		// Special case for MD5
 		if count == 16 {
 			return String(format: "%02x%02x%02x%02x%02x%02x%02x%02x%02x%02x%02x%02x%02x%02x%02x%02x", self[0], self[1], self[2], self[3], self[4], self[5], self[6], self[7], self[8], self[9], self[10], self[11], self[12], self[13], self[14], self[15])
 		}
@@ -125,17 +153,4 @@ public extension Data {
 		return reduce("") { $0 + String(format: "%02x", $1) }
 	}
 
-}
-
-extension String.Encoding {
-	/// A superset of GB-2312.
-	static let GB_18030_2000 = Self.fromCFStringEncodingExt(.GB_18030_2000)
-	static let big5 = Self.fromCFStringEncodingExt(.big5)
-	static let koreanEUC = Self.fromCFStringEncodingExt(.EUC_KR)
-
-	/// Converts a `CFStringEncodings` (note trailing 's') to a `String.Encoding`.
-	static func fromCFStringEncodingExt(_ cfEncoding: CFStringEncodings) -> String.Encoding {
-		let nsEncoding =  CFStringConvertEncodingToNSStringEncoding(CFStringEncoding(cfEncoding.rawValue))
-		return String.Encoding(rawValue: nsEncoding)
-	}
 }
