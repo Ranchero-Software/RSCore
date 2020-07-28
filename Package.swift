f100e41259207a9c2ad66ef17534719c4637dc64
Stuart Breckenridge <stuart.breckenridge@icloud.com>
2020-07-28 21:18:35 +0800

    Changed deployment target to iOS 10

--- Package.swift
+++ Package.swift
@@ -5,7 +5,7 @@ import PackageDescription
 
 let package = Package(
     name: "RSCore",
-    platforms: [.macOS(SupportedPlatform.MacOSVersion.v10_14), .iOS(SupportedPlatform.IOSVersion.v13)],
+    platforms: [.macOS(SupportedPlatform.MacOSVersion.v10_14), .iOS(SupportedPlatform.IOSVersion.v10)],
     products: [
         // Products define the executables and libraries a package produces, and make them visible to other packages.
         .library(
