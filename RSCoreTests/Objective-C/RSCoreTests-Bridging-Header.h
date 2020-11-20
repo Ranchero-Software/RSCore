e8631b9c5157b84d3d793cce4f47add3c337478f
Maurice Parker <mo@vincode.io>
2020-11-20 09:59:34 -0600

    Update to match other ObjC packages layout

--- RSCoreTests/Objective-C/RSCoreTests-Bridging-Header.h
+++
@@ -1,9 +0,0 @@
-//
-//  RSCoreTests-Bridging-Header.h
-//  RSCore
-//
-//  Created by Nate Weaver on 2020-01-18.
-//  Copyright © 2020 Ranchero Software, LLC. All rights reserved.
-//
-
-#import "NSData+RSCore.h"
